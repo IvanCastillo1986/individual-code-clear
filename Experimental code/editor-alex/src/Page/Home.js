43e07e5e24a96d7ad7580212e4625e4eca00e91d
Alexander La Rosa Perez <65190894+alexanderl96-hub@users.noreply.github.com>
2021-09-03 01:30:59 -0400

    Update Home.js

--- Experimental code/editor-alex/src/Page/Home.js
+++ Experimental code/editor-alex/src/Page/Home.js
@@ -4,7 +4,7 @@ import Editor from "@monaco-editor/react"
 export default function Home() {
     return (
         <div>
-            <h3>Hola Coder</h3>
+            <h3>Hello Coder</h3>
             <Editor 
                height="70vh"
                width="70vh"
