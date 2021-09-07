e65465b67eb612e80ad0d9bbe0d8e40c00d357f5
Alexander <alexanderl96@yahoo.com>
2021-09-07 14:49:57 -0400

    progress

--- Experimental code/editor-alex/src/Page/Solution.js
+++ Experimental code/editor-alex/src/Page/Solution.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from "react";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
-export default function Solution() {
-    return (
-        <div className="editor">
-        </div>
-    )
+export default function Solution({ sol }) {
+
+    const [sole, setSole] = useState([]);
+
+    // const handleInpu = (value, sol) => {
+    
+    //     setSole(sol);
+    //   };
+  return (
+    <div>
+      <Editor
+        height="70vh"
+        width="70vh"
+        // onChange={handleInpu}
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        value={sol}
+        className="solution"
+      />
+      {/* <div className="editor" >
+        {sol}
+      </div> */}
+    </div>
+  );
 }
