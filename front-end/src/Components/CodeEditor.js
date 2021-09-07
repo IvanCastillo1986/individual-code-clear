699da2114b604475003b7c519d2ff6d75b5f7471
Ivan Castillo <total180training@gmail.com>
2021-09-07 04:38:05 -0400

    Initial Commit. Front End

---
+++ front-end/src/Components/CodeEditor.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Editor from '@monaco-editor/react'
+
+
+
+export default function CodeEditor() {
+
+    const [input, setInput] = useState('')
+
+    const handleChange = (value, e) => {
+        setInput(value)
+    }
+
+
+    return (
+        <form className='EditorForm'>
+            <Editor 
+                className='Editor'
+                wrapperClassName='EditorWrapper'
+                height='50vh'
+                width='80%'
+                defaultLanguage='javascript'
+                defaultValue='// Some comment'
+                onChange={handleChange}
+            />
+            <input type="submit" value="Submit Code" />
+        </form>
+    )
+}
