f5cacf8ac9201dacd56e1810adf0d65bb95bb6aa
Alexander <alexanderl96@yahoo.com>
2021-09-07 23:28:20 -0400

    work in progress API front-end

--- Experimental code/editor-alex/src/App.js
+++ Experimental code/editor-alex/src/App.js
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react';
 import logo from './call.png'
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"
-
+import axios from 'axios';
+import { apiURL } from './Util/apiURL'
 
 import Home from './Page/Home';
 
+const API_Dtbased = apiURL()
+
 function App() {
+  const [days, setDays] = useState([]);
+
+  // const addCode = (newcode) => {}
+
+  // const deleteCode = (id) => {}
+
+  useEffect(() => {
+    axios.get(`${API_Dtbased}/code`).then((res)=>{
+      const {data} = res
+      console.log(data.payload)
+      setDays(data)
+    })
+  },[])
+ console.log(days)
   return (
     <div className="App">
       <header className="App-header">
