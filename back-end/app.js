4d3bfefe71c197dd741940f11efe82b8433334de
Du Min Chen <dm.chen0113@gmail.com>
2021-09-10 20:48:01 -0400

    Heroku first deployment

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,7 @@
 // DEPENDENCIES
 const cors = require("cors");
 const express = require("express");
-const inputController = require("./Controllers/inputController");
 const eslintController = require("./Controllers/eslintController");
-const { ESLint } = require("eslint");
 
 // CONFIGURATION
 const app = express();
@@ -18,24 +16,11 @@ app.get("/", (req, res) => {
 });
 
 app.use("/eslint", eslintController);
-app.use("/code", inputController);
 
 // 404 Page
 app.get("*", (req, res) => {
   res.status(404).send("Page not found");
 });
-
-// Lint Test
-
-let str = "() => {    const yes = 0      }";
-
-const linter = async () => {
-  const eslint = new ESLint();
-  const results = await eslint.lintText(str);
-  console.log(results[0].messages);
-};
-
-linter();
 
 // EXPORT
 module.exports = app;
