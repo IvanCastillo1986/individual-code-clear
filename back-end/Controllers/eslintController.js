583b0ab64d031541f08a6eb99792c19191b45e3f
Jamee Hengari <jhengari@hotmail.com>
2021-09-16 13:24:02 -0400

    got eslint to fix code

--- back-end/Controllers/eslintController.js
+++ back-end/Controllers/eslintController.js
@@ -2,13 +2,19 @@ const eslint = require("express").Router();
 const { ESLint } = require("eslint");
 
 eslint.get("/", (req, res) => {
-    res.json({ result: "Please use POST method to get a result" });
-})
+  res.json({ result: "Please use POST method to get a result" });
+});
 
 eslint.post("/", async (req, res) => {
-    const lint = new ESLint();
-    const result = await lint.lintText(req.body.input);
-    res.json({ result });
-})
+  const lint = new ESLint();
+  const result = await lint.lintText(req.body.input);
+  res.json({ result });
+});
+
+eslint.post("/fix", async (req, res) => {
+  const lint = new ESLint({ fix: true });
+  const fixedResult = await lint.lintText(req.body.input);
+  res.json({ fixedResult });
+});
 
 module.exports = eslint;
