b100879c5e067f04dc8a7d9ddae27080bbadef14
Jamee Hengari <jhengari@hotmail.com>
2021-09-20 13:47:44 -0400

    backend stats database initial set up

---
+++ back-end/Controllers/statsController.js
@@ -0,0 +1,113 @@
+const stats = require("express").Router();
+const {
+  getAllStats,
+  getStat,
+  createStat,
+  deleteStat,
+  updateStat,
+} = require("../queries/userStats");
+
+stats.get("/", async (req, res) => {
+  try {
+    const allStats = await getAllStats();
+    if (allStats.code === "ECONNREFUSED") {
+      console.log(`Database ${allStats}`);
+      throw `Unable to connect to the database`;
+    } else {
+      res.status(200).json({
+        success: true,
+        payload: allStats,
+      });
+    }
+  } catch (e) {
+    res.status(404).json({
+      error: "Error",
+      message: e,
+    });
+  }
+});
+
+stats.post("/", async (req, res) => {
+  try {
+    const newStat = await createStat(req.body);
+    if (newStat["id"]) {
+      res.status(200).json({
+        success: true,
+        payload: newStat,
+      });
+    } else {
+      console.log(`Database error: ${newStat}`);
+      throw `Error adding ${req.body} to the database`;
+    }
+  } catch (e) {
+    res.status(404).json({
+      error: "Statistic not added",
+      message: e,
+    });
+  }
+});
+
+stats.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const stat = await getStat(id);
+    if (stat["id"]) {
+      res.json({
+        success: true,
+        payload: stat,
+      });
+    } else {
+      console.log(`Database error: ${stat}`);
+      throw `There is no statistic with the id: ${id}`;
+    }
+  } catch (e) {
+    res.status(404).json({
+      error: "Statistic not found",
+      message: e,
+    });
+  }
+});
+
+stats.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedStat = await deleteStat(id);
+    if (deletedStat["id"]) {
+      res.status(200).json({
+        success: true,
+        payload: deletedStat,
+      });
+    } else {
+      console.log(`Database error: ${deletedStat}`);
+      throw `There is no statistic to delete with the id: ${id}`;
+    }
+  } catch (e) {
+    res.status(404).json({
+      error: "Statistic not deleted",
+      message: e,
+    });
+  }
+});
+
+stats.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedStat = await updateStat(id, req.body);
+    if (updatedStat["id"]) {
+      res.status(200).json({
+        success: true,
+        payload: updatedStat,
+      });
+    } else {
+      console.log(`Database error: ${updatedStat}`);
+      throw `There is no statistic to be updated with the id: ${id}`;
+    }
+  } catch (e) {
+    res.status(404).json({
+      error: "Statistic not updated",
+      message: e,
+    });
+  }
+});
+
+module.exports = stats;
