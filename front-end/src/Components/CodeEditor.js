1628c71d1750d4200722bf359754bed06e2635ac
Du Min Chen <dm.chen0113@gmail.com>
2021-10-01 17:23:22 -0400

    Add import code from js file to editor support

--- front-end/src/Components/CodeEditor.js
+++ front-end/src/Components/CodeEditor.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
 
 export default function CodeEditor({
   handleChange,
   handleSubmit,
   handleEditorDidMount,
+  monacoObjects
 }) {
+  const [fileText, setFileText] = useState(null);
+
+  const getFile = async (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+
+    reader.onload = async (e) => setFileText(e.target.result);
+    reader.readAsText(e.target.files[0]);
+  }
+
+  const handleFileSubmit = (e) => {
+    e.preventDefault();
+    monacoObjects.current.editor.setValue(fileText);
+  }
+
   return (
     <div className="CodeEditor">
       <h2>Code Editor</h2>
+      <form onSubmit={handleFileSubmit}>
+        <label for="file">
+          <input type="file" accept=".js" minSize={0} maxSize={5242880} onChange={getFile} />
+        </label>
+        <input type="submit" value="Import code from js file" />
+      </form>
       <form action="" onSubmit={handleSubmit}>
         <Editor
           className="Editor"
